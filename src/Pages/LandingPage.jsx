1041d9d1c865be56bb9aa113791ce900d989d762
Flomoku231 <flomokuemmanuel114@gmail.com>
2022-08-25 10:47:50 +0200

    ProjectSetup

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styles/LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div>
+        <nav>
+            <h1>landing Page</h1>
+        </nav>
+    </div>
+  )
+}
+
+export default LandingPage
